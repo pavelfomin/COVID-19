5dcc53c4f7b52f499907a1a65958658943d192ff
Pavel Fomin <p.v.fomin@gmail.com>
2020-04-07 14:03:09 -0500

    4/6 update

--- scripts/aggregate-us-rotated.sh
+++ scripts/aggregate-us-rotated.sh
@@ -17,3 +17,4 @@ $cmd $inputDir/04-02-2020.csv State Rotate $filter | tail -n+2 >> $output
 $cmd $inputDir/04-03-2020.csv State Rotate $filter | tail -n+2 >> $output
 $cmd $inputDir/04-04-2020.csv State Rotate $filter | tail -n+2 >> $output
 $cmd $inputDir/04-05-2020.csv State Rotate $filter | tail -n+2 >> $output
+$cmd $inputDir/04-06-2020.csv State Rotate $filter | tail -n+2 >> $output
